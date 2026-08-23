23a6c5fb2bca699d16294eb7c00d6097a0089b1d
116-MSB <brueckmatze@googlemail.com>
2026-08-23 22:49:19 +0200

    Update supabase-test.js

--- supabase-test.js
+++ supabase-test.js
@@ -1,37 +1,5 @@
 document
 .getElementById("saveBtn")
-.addEventListener("click", async () => {
-
-try{
-
-const response = await fetch(
-"https://bhbolkpumkmhusenbhcz.supabase.co/rest/v1/guesses",
-{
-method: "POST",
-headers: {
-"apikey": "sb_publishable_Y-CZrSOy5hqtydErQP26DA_oZI4ydZw",
-"Authorization": "Bearer sb_publishable_Y-CZrSOy5hqtydErQP26DA_oZI4ydZw",
-"Content-Type": "application/json",
-"Prefer": "return=minimal"
-},
-body: JSON.stringify({
-guest_name: "TEST",
-glas1: 1,
-glas2: 2,
-glas3: 3,
-glas4: 4,
-glas5: 5,
-total_score: 0
-})
-}
-);
-
-alert("Status: " + response.status);
-
-}catch(err){
-
-alert("Fehler: " + err.message);
-
-}
-
+.addEventListener("click", () => {
+  alert("Button funktioniert");
 });
